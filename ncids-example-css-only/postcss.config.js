916808cf041fd79bf92aa29c6df1c6cbb32490a1
Olivia Tharp <olivia.tharp@nih.gov>
2023-06-27 11:53:23 -0500

    add autoprefixer to fix svgs

--- ncids-example-css-only/postcss.config.js
+++ ncids-example-css-only/postcss.config.js
@@ -1,3 +1,9 @@
+/*
+ PostCSS plugin to parse CSS and add vendor prefixes to CSS rules using values.
+ Autoprefixer will use the data based on current browser popularity and
+ property support to apply prefixes.
+ */
+
 module.exports = {
 	plugins: [require('autoprefixer')],
 };
