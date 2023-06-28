4a1711c49d9b2a2f87c44f19b68b426527b1df7a
Olivia Tharp <olivia.tharp@nih.gov>
2023-06-28 14:58:15 -0500

    add prod optimizations

--- ncids-example-css-only/webpack.config.js
+++ ncids-example-css-only/webpack.config.js
@@ -1,53 +1,79 @@
 const webpack = require('webpack');
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                includePaths: [
-                  path.join(
-                      __dirname,
-                      './node_modules/@nciocpl/ncids-css/packages'
-                  ),
-                  path.join(
-                      __dirname,
-                      './node_modules/@nciocpl/ncids-css/uswds-packages'
-                  ),
-                ],
-              },
-              sourceMap: true,
-            },
-          },
-        ]
-      },
-    ]
-  }
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
+
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(scss|css)$/,
+					use: [
+						isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+						isProduction
+							? {
+									loader: 'css-loader',
+									options: {
+										importLoaders: 2,
+										sourceMap: false,
+									},
+							  }
+							: 'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								sourceMap: !isProduction,
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sassOptions: {
+									includePaths: [path.join(__dirname, './node_modules/@nciocpl/ncids-css/packages'), path.join(__dirname, './node_modules/@nciocpl/ncids-css/uswds-packages')],
+								},
+								sourceMap: !isProduction,
+							},
+						},
+					],
+				},
+				{
+					test: /\.svg/,
+					type: 'asset/inline',
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: 'src/index.html',
+				inject: 'head',
+				minify: false,
+				meta: {
+					charset: { charset: 'UTF-8' },
+				},
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css',
+			}),
+		],
+		optimization: isProduction
+			? {
+					minimize: true,
+					minimizer: [new CssMinimizerPlugin(), '...'],
+					runtimeChunk: {
+						name: 'runtime',
+					},
+			  }
+			: {},
+	};
 };
-
-module.exports = config;
